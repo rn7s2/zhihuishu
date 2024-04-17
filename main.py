d2aa346ec467d52faf5f37c4095d2c6834fa1762
rn7s2 <mail@rn7s2.cn>
2024-04-17 13:16:12 +0800

    Init.

---
+++ main.py
@@ -0,0 +1,58 @@
+import datetime
+import time
+import pyautogui
+import util
+
+util.moveToMenu()
+
+watchedSecs = 0
+while watchedSecs < 28 * 60:
+    selected = False
+    
+    lst = list(pyautogui.locateAllOnScreen("assets/vidBtn.png"))
+    for pos in lst:
+        doneIcon = pyautogui.screenshot(region=(int(pos.left + 275), int(pos.top - 18), 28, 28))
+        if not util.blankImage(doneIcon):
+            continue
+        
+        selected = True
+    
+        vidLenImg = pyautogui.screenshot(region=(int(pos.left + 20), int(pos.top), 75, 20))
+        vidSecs, vidLen = util.ocrVidSecs(vidLenImg)
+        print(f"selected video, len={vidLen}, secs={vidSecs}")
+    
+        pyautogui.click(pos.left + 20, pos.top, duration=util.moveDuration(), tween=pyautogui.easeInOutQuad)
+        time.sleep(3)
+        util.startPlayVideo()
+
+        finishTime = datetime.datetime.now() + datetime.timedelta(seconds=vidSecs + 60)
+        print(f"start watching video, will finish at {finishTime}")
+
+        while datetime.datetime.now() < finishTime:
+            if pyautogui.locateOnScreen("assets/singleQuiz.png") != None:
+                print("quiz (single)")
+                btns = list(pyautogui.locateAllOnScreen("assets/singleQuizBtn.png"))
+                for btn in btns:
+                    pyautogui.click(btn.left + 10, btn.top + 5, duration=util.moveDuration(), tween=pyautogui.easeInOutQuad)
+                    time.sleep(0.2)
+
+                    try:
+                        if pyautogui.locateOnScreen("assets/singleQuizPass.png") != None:
+                            print("quiz pass, close")
+                            util.closeSingleQuiz()
+                            util.startPlayVideo()
+
+                            break
+                    except:
+                        print("wrong answer")
+                        time.sleep(0.2)
+                        continue
+            
+            # else if 
+
+            time.sleep(3)
+    
+        break
+    
+    if not selected:
+        util.scrollDownVidsList()
---
+++ util.py
@@ -0,0 +1,59 @@
+import random
+import pyautogui
+from cnocr import CnOcr
+import time
+
+ocr = CnOcr()
+
+def moveDuration():
+    return random.uniform(0.2, 0.8)
+
+def moveToMenu():
+    menuPos = pyautogui.locateOnScreen("assets/menu.png")
+    pyautogui.moveTo(menuPos.left + int(random.random() * 15),
+                     menuPos.top + int(random.random() * 15),
+                     duration=moveDuration(),
+                     tween=pyautogui.easeInOutQuad)
+
+def scrollDownVidsList():
+    print("scroll down")
+    menuPos = pyautogui.locateOnScreen("assets/menu.png")
+    pyautogui.moveTo(menuPos.left + int(random.random() * 100),
+                     menuPos.top + 50 + int(random.random() * 250),
+                     duration=moveDuration(),
+                     tween=pyautogui.easeInOutQuad)
+    pyautogui.scroll(-240 - int(random.random() * 120))
+    time.sleep(0.5)
+
+def blankImage(img):
+    bytes = img.tobytes()
+
+    white = True
+    for byte in bytes:
+        if byte != 255:
+            white = False
+            break
+
+    return white
+
+def ocrVidSecs(vidLenImg) -> tuple[int, str]:
+    out = ocr.ocr(vidLenImg)[0].get("text")
+    out = "".join(filter(lambda x: x.isdigit(), out))
+    if len(out) < 6:
+        out = out + "0"*(6-len(out))
+
+    h = int(out[0:2])
+    m = int(out[2:4])
+    s = int(out[4:6])
+
+    out = f"{h:02d}:{m:02d}:{s:02d}"
+
+    return h * 3600 + m * 60 + s, out
+
+def closeSingleQuiz():
+    closeBox = pyautogui.locateOnScreen("assets/singleQuizClose.png")
+    pyautogui.click(closeBox.left, closeBox.top, duration=moveDuration(), tween=pyautogui.easeInOutQuad)
+
+def startPlayVideo():
+    pyautogui.click(random.randint(300, 900),
+                    random.randint(300, 600))
