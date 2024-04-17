a40226f7b893da148d847a023a5a91c36dc7f2e9
rn7s2 <mail@rn7s2.cn>
2024-04-17 14:13:01 +0800

    Handle exception raised by finding quiz.

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ util.moveToMenu()
 watchedSecs = 0
 while watchedSecs < 28 * 60:
     selected = False
-    
+
     lst = list(pyautogui.locateAllOnScreen("assets/vidBtn.png"))
     for pos in lst:
-        doneIcon = pyautogui.screenshot(region=(int(pos.left + 275), int(pos.top - 18), 28, 28))
+        doneIcon = pyautogui.screenshot(
+            region=(int(pos.left + 275), int(pos.top - 18), 28, 28)
+        )
         if not util.blankImage(doneIcon):
             continue
-        
+
         selected = True
-    
-        vidLenImg = pyautogui.screenshot(region=(int(pos.left + 20), int(pos.top), 75, 20))
+
+        vidLenImg = pyautogui.screenshot(
+            region=(int(pos.left + 20), int(pos.top), 75, 20)
+        )
         vidSecs, vidLen = util.ocrVidSecs(vidLenImg)
         print(f"selected video, len={vidLen}, secs={vidSecs}")
-    
-        pyautogui.click(pos.left + 20, pos.top, duration=util.moveDuration(), tween=pyautogui.easeInOutQuad)
+
+        pyautogui.click(
+            pos.left + 20,
+            pos.top,
+            duration=util.moveDuration(),
+            tween=pyautogui.easeInOutQuad,
+        )
         time.sleep(3)
         util.startPlayVideo()
 
@@ -29,30 +38,41 @@ while watchedSecs < 28 * 60:
         print(f"start watching video, will finish at {finishTime}")
 
         while datetime.datetime.now() < finishTime:
-            if pyautogui.locateOnScreen("assets/singleQuiz.png") != None:
-                print("quiz (single)")
-                btns = list(pyautogui.locateAllOnScreen("assets/singleQuizBtn.png"))
-                for btn in btns:
-                    pyautogui.click(btn.left + 10, btn.top + 5, duration=util.moveDuration(), tween=pyautogui.easeInOutQuad)
-                    time.sleep(0.2)
-
-                    try:
-                        if pyautogui.locateOnScreen("assets/singleQuizPass.png") != None:
-                            print("quiz pass, close")
-                            util.closeSingleQuiz()
-                            util.startPlayVideo()
-
-                            break
-                    except:
-                        print("wrong answer")
+            try:
+                if pyautogui.locateOnScreen("assets/singleQuiz.png") != None:
+                    print("quiz (single)")
+                    btns = list(pyautogui.locateAllOnScreen("assets/singleQuizBtn.png"))
+                    for btn in btns:
+                        pyautogui.click(
+                            btn.left + 10,
+                            btn.top + 5,
+                            duration=util.moveDuration(),
+                            tween=pyautogui.easeInOutQuad,
+                        )
                         time.sleep(0.2)
-                        continue
-            
-            # else if 
+
+                        try:
+                            if (
+                                pyautogui.locateOnScreen("assets/singleQuizPass.png")
+                                != None
+                            ):
+                                print("quiz pass, close")
+                                util.closeSingleQuiz()
+                                util.startPlayVideo()
+
+                                break
+                        except:
+                            print("wrong answer")
+                            time.sleep(0.2)
+                            continue
+            except:
+                pass
+
+            # else if
 
             time.sleep(3)
-    
+
         break
-    
+
     if not selected:
         util.scrollDownVidsList()
